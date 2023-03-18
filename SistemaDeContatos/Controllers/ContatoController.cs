b0396d87be349e3996318919ed5f11ecd2d95e36
Mateus Falcão <mateus.souza.falcao@gmail.com>
2023-03-17 22:10:50 -0300

    dia 2 finalizado a primeira parte do front

--- SistemaDeContatos/Controllers/ContatoController.cs
+++ SistemaDeContatos/Controllers/ContatoController.cs
@@ -16,7 +16,7 @@ namespace SistemaDeContatos.Controllers
         {
             return View();
         }
-        public IActionResult Apagar()
+        public IActionResult ApagarConfirmacao()
         {
             return View();
         }
